614cd24ac4509ae2c88e9d55a53353c92396a53a
Radoslav Tsonev <radoslavtsonev01@gmail.com>
2022-03-31 22:59:29 +0300

    Part 3

---
+++ 11.While loops.py
@@ -0,0 +1,9 @@
+#while loop = a statement that will execute it's bloack of code, as long as it's condition remains true
+
+#while 1==1:
+    #name = print("Help! I'm stuck in a loop!")
+print("-------------------------------")
+name= ""
+while len(name)==0:
+    name=input("Enter your name ")
+print("Hello"+name)
---
+++ 12.For loops.py
@@ -0,0 +1,16 @@
+#for loop =   a statement that will execute it's bloack of code
+#             a limited amount of times
+#while loop = unlimited or infinite
+#for loop =
+
+for i in range(10):
+    print(i+1)
+#prints  all numbers from 1 to 10
+
+for i in range (50, 100+1, 2):
+    # start 50 to the end of 101 and the difference between the numbers
+    print(i)
+#prints the numbers from 50 to 100 with the difference 2
+for i in "Bro Code":
+    print(i)
+    #prints all numbers , each one on the different line
